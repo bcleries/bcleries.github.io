bad8f70a875a9a8b6a43b3f35ce9b0482e72323e
Borja Cleries <48927872+bcleries@users.noreply.github.com>
2026-09-16 11:29:31 +0200

    Add event listener to change title on click

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
 
+    const titulo = document.querySelector(".hero h1");
+
+    titulo.addEventListener("click", function () {
+
+        titulo.textContent = "¡Bienvenido! 🚀";
+
+    });
+
+});
